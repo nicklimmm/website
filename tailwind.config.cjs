1e12d14ce4791f75cf2ad8af66ef0d85a0480953
Nicky Lim <nickylim.p@gmail.com>
2023-07-14 02:26:22 +0800

    Set default font, color palette, modify eslint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: '#1572A1',
+				secondary: '#9AD0EC',
+				tertiary: '#EFDAD7',
+				accent: '#E3BEC6',
+			},
+		},
 	},
 	plugins: [],
 }
