d87eead27faed7c33153cf7edec066850511eebd
Nicky Lim <nickylim.p@gmail.com>
2024-06-09 18:14:47 +0700

    Update content and use fontsource

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+				sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				primary: '#1572A1',
